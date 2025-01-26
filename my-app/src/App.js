e5c1954a756283771bca045d6a423abd085e0ba7
vozmitelvlas <vozmitel.vlas228@mail.ru>
2025-01-26 23:05:26 +0300

    reactApp

---
+++ my-app/src/App.js
@@ -0,0 +1,31 @@
+//императивный
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+
+    return (
+        //begin декларативный
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <p>{new Date().getFullYear()}</p>
+            </header>
+        </div>
+        //end декларативный
+    );
+}
+
+// императивный
+export default App;

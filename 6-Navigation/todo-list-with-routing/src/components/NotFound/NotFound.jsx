80f2777760b2004dc75852e805cc57c981a97fbe
vozmitelvlas <vozmitel.vlas228@mail.ru>
2025-03-30 11:04:03 +0300

    route todos

---
+++ 6-Navigation/todo-list-with-routing/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+export default function NotFound(){
+    return(
+        <>
+            <div>
+                <h1>404 error</h1>
+                Такой страницы не существует
+            </div>
+        </>
+    )
+}
---
+++ 6-Navigation/todo-list-with-routing/src/components/Slider/SliderButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./SliderButton.module.css";
+
+export default function SliderButton ({setOpenerSlider, openerSlider, sort}) {
+    const toggleMode = () => {
+        sort(!openerSlider)
+    }
+    return (
+        <div>
+            <div onClick={toggleMode} className={`${styles.slider} ${openerSlider ? styles.sliderOn : styles.sliderOff}`}>
+                <div className={`${styles.circle} ${openerSlider ? styles.circleOn : styles.circleOff}`}/>
+            </div>
+        </div>
+    )
+}

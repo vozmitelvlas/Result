7e48a328adf6468983bf55dec9bf0a58f800cec2
vozmitelvlas <vozmitel.vlas228@mail.ru>
2026-07-12 12:38:09 +1000

    feat: add Location and Episodes page

--- React/react-router/src/routes/Locations.tsx
+++ React/react-router/src/routes/Locations.tsx
@@ -1,14 +1,29 @@
 import styled from 'styled-components';
-import {type StyledProps} from "../types";
+import type {LocationsProps, StyledProps} from "../types";
+import {useLoaderData} from "react-router";
 
 const LocationsPageContainer = ({className}: StyledProps) => {
+    const locations = useLoaderData();
     return (
         <div className={className}>
-            <h2>Расположение</h2>
+            <h1>Локации</h1>
+            {locations.map(({id, name}: LocationsProps) => (
+                <div className="location" key={id}>
+                    <h3>{name}</h3>
+                </div>
+            ))}
+
         </div>
     );
 };
 
 export const LocationsPage = styled(LocationsPageContainer)`
+  .location {
+    display: flex;
+    margin-bottom: 5px;
+    justify-content: center;
+    border: solid 2px #000;
+    border-radius: 8px;
+  }
 `;
 

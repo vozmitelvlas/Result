d56cc3728d098f021ab63e8246a70f11cfcbd0cd
vozmitelvlas <vozmitel.vlas228@mail.ru>
2026-07-12 12:38:09 +1000

    feat: add Location and Episodes page

--- React/react-router/src/routes/Episodes.tsx
+++ React/react-router/src/routes/Episodes.tsx
@@ -1,13 +1,29 @@
-import {type StyledProps} from "../types";
+import type {EpisodeProp, StyledProps} from "../types";
+import {useLoaderData} from "react-router";
 import styled from "styled-components";
 
 const EpisodesPageContainer = ({className}: StyledProps) => {
+    const episodes = useLoaderData();
+
     return (
         <div className={className}>
+            <h1>Эпизоды</h1>
+            {episodes.map(({id, name}: EpisodeProp) => (
+                <div className="episode" key={id}>
+                    <h3>{name}</h3>
+                </div>
+            ))}
             <h2>Эпизоды</h2>
         </div>
     );
 };
 
 export const EpisodesPage = styled(EpisodesPageContainer)`
+  .episode {
+    display: flex;
+    margin-bottom: 5px;
+    justify-content: center;
+    border: solid 2px #000;
+    border-radius: 8px;
+  }
 `;
---
+++ React/react-router/src/types/categories.ts
@@ -0,0 +1,26 @@
+export interface LocationsProps {
+    id: number,
+    name: string,
+    type: string,
+    dimension: string,
+    created: string,
+}
+
+export interface HeroProps {
+    id: number,
+    name: string,
+    status: string,
+    species: string,
+    type: string,
+    gender: string,
+    image: string,
+    created: string
+}
+
+export interface EpisodeProp {
+    id: number,
+    name: string,
+    air_date: string,
+    episode: string,
+    created: string,
+}

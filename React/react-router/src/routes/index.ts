1dc6c246c08fb761c91cd122f49a3c977623eab2
vozmitelvlas <vozmitel.vlas228@mail.ru>
2026-07-27 11:53:43 +1000

    feat: add sortDropDown with validating parametres

--- React/react-router/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Heroes.tsx";
-export * from "./Locations.tsx";
-export * from "./Episodes.tsx";
-export * from "./Hero.tsx";
-export * from "./Episode.tsx";
-export * from "./Location.tsx";
--- React/react-router/src/types/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./styled.ts";
-export * from "./categories.ts";

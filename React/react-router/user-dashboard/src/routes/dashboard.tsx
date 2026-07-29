1bc9a6db972367159feaa01c0c43f2623376977c
vozmitelvlas <vozmitel.vlas228@mail.ru>
2026-07-29 14:27:00 +1000

    feat: add stats, settings, profile pages

--- React/react-router/user-dashboard/src/routes/dashboard.tsx
+++ React/react-router/user-dashboard/src/routes/dashboard.tsx
@@ -1,12 +1,23 @@
-import {useLoaderData} from "react-router";
+import {NavLink, Outlet, useLoaderData} from "react-router";
 
 export const Dashboard = () => {
     const {name} = useLoaderData();
 
     return (
         <div>
-            <h1>It is DashBoard</h1>
-            <h2>User name is {name}</h2>
+            <header>
+                <h1>Привет, {name}</h1>
+                <nav className="flex gap-2 justify-center m-6">
+                    <NavLink to="profile" className="bg-amber-200 p-2 rounded-xl border">Профиль</NavLink>
+                    <NavLink to="settings" className="bg-amber-200 p-2 rounded-xl border">Настройки</NavLink>
+                    <NavLink to="stats" className="bg-amber-200 p-2 rounded-xl border">Все данные</NavLink>
+                </nav>
+            </header>
+
+            <main>
+                <Outlet/>
+            </main>
         </div>
     );
 };
+
